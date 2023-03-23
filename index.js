5657c9b3c3f521bda6b11a87f61bfa06a95ccbf3
Scarlet234 <109751916+Scarlet234@users.noreply.github.com>
2023-03-22 22:08:31 -0600

    added inquirer prompts and additional functions with sql prompts

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const db = mysql.createConnection(
     // MySQL username,
     user: 'root',
     // TODO: Add MySQL password here
-    password: '',
+    password: 'sunday',
     database: 'workforce_db'
   },
   console.log(`Connected to the workforce_db database.`)
@@ -69,27 +69,161 @@ function init() {
             }
         });
 }
-function viewDepartments() {
-    db.query('SELECT * FROM department', function (err, results) {
+const viewDepartments = () => {
+    db.query('SELECT * FROM department', (err, results) => {
+        cTable(results);
+        init();
+    });
+};
+
+viewRoles = () => {
+    db.query('SELECT * FROM role', (err, results) => {
         cTable(results);
         init();
     });
 }
 
-  const sql = `INSERT INTO employee (first_name)
-    VALUES (?)`;
-  const params = "Scarlet"
-  
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: body
+viewEmployees = () => {
+    db.query('SELECT * FROM role', (err, results) => {
+        cTable(results);
+        init();
     });
-  });
+}
+
+addDepartment = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the name of the department you want to add?'
+                
+            }
+        ])
+        .then((answer) => {
+            db.query('INSERT INTO department SET ?', answer, (err, results) => {    
+                if (err) {
+                    console.log(err);
+                }   
+                console.log('Department added successfully!');
+                showDepartments();
+                init();
+            });
+        });
+}
+
+addRole = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What is the title of the role you want to add?'
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: 'What is the salary of the role you want to add?'
+            },
+            {
+                type: 'input',
+                name: 'department_id',
+                message: 'What is the department ID of the role you want to add?'
+            }
+        ])
+        .then((answer) => {
+            db.query('INSERT INTO role SET ?', answer, (err, results) => {
+                if (err) {
+                    console.log(err);
+                }
+                console.log('Role added successfully!');
+                showRoles();
+                init();
+            });
+        });
+}
+
+addEmployee = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'first_name',
+                message: 'What is the first name of the employee you want to add?'
+                
+            },
+            {
+                type: 'input',
+                name: 'last_name',
+                message: 'What is the last name of the employee you want to add?'
+
+            },
+            {
+                type: 'input',
+                name: 'role_id',
+                message: 'What is the role ID of the employee you want to add?'
+
+            },
+            {
+                type: 'input',
+                name: 'manager_id',
+                message: 'What is the manager ID of the employee you want to add?'
+
+            }
+        ])
+        .then((answer) => {
+            db.query('INSERT INTO employee SET ?', answer, (err, results) => {
+                if (err) {
+                    console.log(err);
+                }
+                console.log('Employee added successfully!');
+                showEmployees();
+                init();
+            });
+        });
+}
+
+updateEmployeeRole = () => {
+    // get employees from employee table
+    const employeeSql = db.query `SELECT * FROM employee`;
+
+    Connection.promise().query(employeeSql, (err, results) => {
+        if (err) throw err;
+
+    const employeeChoices = results.map(({ id, first_name, last_name }) => ({id: id, name: `${first_name} ${last_name}`}));
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the ID of the employee you want to update?'
+                
+            },
+            {
+                type: 'input',
+                name: 'role_id',
+                message: 'What is the new role ID of the employee you want to update?'
+
+            }
+        ])
+        .then((answer) => {
+            db.query('UPDATE employee SET ? WHERE ?', [answer, {id: answer.id}], (err, results) => {
+                if (err) {
+                    console.log(err);
+                }
+                console.log('Employee updated successfully!');
+                showEmployees();
+                init();
+            });
+        });
+}
+    )
+}
+
+quit = () => {
+    console.log('Goodbye!');
+    process.exit();
+}
 
 
 //updateEmployeeRole();
@@ -97,98 +231,4 @@ function viewDepartments() {
 //2. before asking which employee, you need to know all the employees, which means you need to make a SELECT * from employee query
 //3. map over (or for loop over) the results array and return an array with the employee names
 //4. once you have that formatted array, you can use it as your choices in the inquirer prompt
-
-
-// team
-const teamCards = [];
-
-// add employee
-const addEmployee = ()=>{
-    inquirer
-    .prompt([
-        {
-            type:"input",
-            name:"name",
-            message:"What is the name of the employee on the team?",
-        },
-
-        {
-            type:"list",
-            name:"role",
-            choices:["Engineer", "Intern"]
-        },
-        
-        {
-            type:"input",
-            name:"id",
-            message:"What is the employee ID?",
-        },
-        {
-            type:"input",
-            name:"email",
-            message:"what is the email address?",
-        },
-        {
-            type:"input",
-            name:"github",
-            when: (input) => input.role === "Engineer",
-            message:"What is the Github username of the engineer?",
-        },
-        {
-            type:"input",
-            name:"school",
-            when: (input) => input.role === "Intern",
-            message:"What is the school of the intern?",
-        },
-        {
-            type: "confirm",
-            name:"oneMore",
-            message:"Would you like to add one more employee to the team?",
-            default: false
-        }
-    ])
-    .then((employeeInput)=>{
-        let {name, id, email, role, github, school, oneMore} = employeeInput;
-        let employee;
-        
-        // engineer
-        if (role === "Engineer"){
-            employee = new Engineer (name, id, email, github);
-            console.log(employee)
-        }
-        
-        // intern
-        if (role === "Intern"){
-            employee = new Intern (name, id, email, school);
-            console.log(employee)
-        }
-
-        teamCards.push(employee);
-
-        // if adding one more employee, run addEmployee again
-        if (oneMore) {
-            addEmployee(teamCards);
-        }
-        else{
-            generate();
-        }
-    })
-}
-
-// function to generate the HTML
-
-const generate = () => {
-    fs.writeFile('./dist/index.html', generateHTML(teamCards), (err) => {
-        if (err) {
-            console.log(err);
-          } else {
-            console.log("Your team profile has been generated successfully!");
-           }
-    })
-    }
-
-const startApp = () => {
-    addManager();
-}
-startApp();
-
+//5. once you have the employee name, you need to get the employee id, which means you need to make another query to get the employee id
